e7cb49ff8c18d1e3373bd2384f5c61bb0d05f50e
mielecarmine <mielecarmine993@gmail.com>
2024-05-22 17:29:44 +0200

    mod ProjectCard update main js

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -25,10 +25,14 @@ export default {
   <div class="card" style="width: 18rem; height: 300px;" >
     <!-- <img src="..." class="card-img-top" alt="..." /> -->
     <div class="card-body">
-      <h5 class="card-title">{{ this.project.name }}</h5>
+      <h4 class="card-title">{{ this.project.name }}</h4>
+      <hr>
       <p class="card-text">
         {{ this.getAbstract(this.project.description) }}
       </p>
+      <span v-if="this.project.type.label == 'Frontend'"  class="badge-frontend">{{ this.project.type.label }}</span>
+      <span v-else-if="this.project.type.label == 'Backend'"  class="badge-backend">{{ this.project.type.label }}</span>
+      <span v-else-if="this.project.type.label == 'Full-stack'"  class="badge-fullstack">{{ this.project.type.label }}</span>
       
       <a href="<!-- BIND SHOW ROUTE -->" class="btn btn-primary">Mostra dettaglio</a>
     </div>
@@ -36,4 +40,44 @@ export default {
 </template>
 <style lang="scss">
 @use "../styles/general.scss";
+
+.badge-frontend {
+  display: inline-block;
+  width: 50%;
+  text-align: center;
+  vertical-align: baseline;
+  border-radius: 10px;
+  line-height: 1;
+  padding: 5px 15px;
+  background-color: #a51ad6;
+  color: white;
+  margin-bottom: 10px;
+}
+
+.badge-backend {
+  display: inline-block;
+  width: 50%;
+  text-align: center;
+  vertical-align: baseline;
+  border-radius: 10px;
+  line-height: 1;
+  padding: 5px 15px;
+  background-color: #b6bc90;
+  color: white;
+  margin-bottom: 10px;
+}
+
+.badge-fullstack {
+  display: inline-block;
+  width: 50%;
+  text-align: center;
+  vertical-align: baseline;
+  border-radius: 10px;
+  line-height: 1;
+  padding: 5px 15px;
+  background-color: #9096bb;
+  color: white;
+  margin-bottom: 10px;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,45 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-createApp(App).mount("#app");
 
 // import boostrapp css
 import "bootstrap/dist/css/bootstrap.css";
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+    faDatabase,
+} from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { 
+    faHtml5,
+    faCss3Alt,
+    faJs,
+    faPhp,
+    faGit,
+    faLaravel
+ } from "@fortawesome/free-brands-svg-icons";
+
+/* add icons to the library */
+library.add(
+    faHtml5,
+    faCss3Alt,
+    faDatabase,
+    faJs,
+    faPhp,
+    faGit,
+    faLaravel
+);
+
+// import { createApp } from "vue";
+// import App from "./App.vue";
+// createApp(App).mount("#app");
+// app.component("font-awesome-icon", FontAwesomeIcon);
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+// app.use(router);
+app.mount("#app");
